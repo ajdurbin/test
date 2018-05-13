8a9e956696691c59240a0f3654c2921bbdc8e492
Alexander Durbin <ajdurbin2@gmail.com>
2018-05-13 13:26:15 +0000

    docker hub push

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "DOCKER_USERNAME" --password-stdin
-docker push ajdurbin/test
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+export APP=test
+export REPO=$DOCKER_USER/$APP
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+docker build -f Dockerfile -t $REPO:$TAG -t $REPO:travis-$TRAVIS_BUILD_NUMBER .
+docker push $REPO:$TAG
+docker push $REPO:travis-$TRAVIS_BUILD_NUMBER
